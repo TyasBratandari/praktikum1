236bb4922556f47bb3c5960ccaa7d9e82ebb9fe6
Tyas Bratandari <tyas_bratandari_29rpl@student.smktelkom-mlg.sch.id>
2022-04-28 04:14:04 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import Main from "./Main";
+import { Link } from "react-router-dom";
+import { Nav } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
+                    <div className="container-fluid">
+                        <span className="navbar-brand fw-bold fs-4 ms-5">Praktikum 1</span>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                                {/* <li className="nav-item">
+                                    <Link className="nav-link" to="/Bank">Bank</Link>
+                                </li> */}
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/BMI">BMI</Link>
+                                </li>
+                                {/* <li className="nav-item">
+                                    <Link className="nav-link" to="/Harga">Harga</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/Biner">Biner</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/Decimal">Decimal</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/Hexadecimal">Hexadecimal</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/Octal">Octal</Link>
+                                </li> */}
+                            </ul>
+                        </div>
+                    </div>
+                </Nav>
+                <Main />
+            </div>
+        );
+    }
 }
 
 export default App;
